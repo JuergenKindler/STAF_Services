2855095bc9ec5ce100ddbeaa128183ee92488747
JuergenKindler <jkindler@freenet.de>
2011-02-16 16:59:56 +0100

    base poms

---
+++ irc/src/main/java/org/jki/staf/service/App.java
@@ -0,0 +1,13 @@
+package org.jki.staf.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
